76833fdd0343064f61bf00f0019f7cbcbcd8ef30
Valkhnut <dhjob@yandex.by>
2024-03-08 14:34:23 +0300

    views created

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import Http404
+
+from .models import Post
+
 
 # Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/list.html',
+                  {
+                      'posts': posts,
+                  }
+                  )
+
+def post_detail(request):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404('No Post Found')
+
+    return render(
+        request,
+        'blog/post/detail.html',
+        {
+            'post': post
+        }
+    )

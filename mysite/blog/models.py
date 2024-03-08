351ac955bd886de04e92630e00c05ea19cdabf60
Valkhnut <dhjob@yandex.by>
2024-03-08 13:16:42 +0300

    add Post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(
+        max_length=155,
+        verbose_name='Title'
+    )
+
+    slug = models.SlugField(
+        max_length=250,
+        verbose_name='SLUG'
+    )
+
+    body = models.TextField(
+
+    )
+
+    def __str__(self):
+        return self.title

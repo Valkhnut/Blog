eb77ea728a310f283938c30bd706071b04574d29
Valkhnut <dhjob@yandex.by>
2024-03-08 13:29:38 +0300

    update for Post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.utils import timezone
+
 
 # Create your models here.
 class Post(models.Model):
@@ -14,8 +16,31 @@ class Post(models.Model):
     )
 
     body = models.TextField(
-
+        verbose_name='Content'
     )
+
+    publish = models.DateTimeField(
+        default=timezone.now,
+        verbose_name='Published date'
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Created date'
+    )
+
+    updated = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Updated date'
+    )
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(
+                fields=['-publish']
+            )
+        ]
 
     def __str__(self):
         return self.title

d6d88f5c75759b1eea1d93c6717d125d1e976bed
Valkhnut <dhjob@yandex.by>
2024-03-09 13:17:54 +0300

    added share by mail functional

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email server configuration
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'djangosite.test.mailler@gmail.com'
+EMAIL_HOST_PASSWORD = 'ingd dljn ohhm ymmw'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True

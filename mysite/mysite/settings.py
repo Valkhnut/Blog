6d664ad5aafa050f89d3eb8c9ac27f6fa0d8155c
Valkhnut <dhjob@yandex.by>
2024-03-09 20:26:32 +0300

    add comments functional

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -127,7 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email server configuration
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'djangosite.test.mailler@gmail.com'
-EMAIL_HOST_PASSWORD = 'ingd dljn ohhm ymmw'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True

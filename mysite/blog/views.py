8cb310240e9a271eaa4bc11d234caa0c698438a2
Valkhnut <dhjob@yandex.by>
2024-03-09 20:24:10 +0300

    add comments functional

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 # Create your views here.
@@ -35,9 +36,20 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day,
                              status=Post.Status.PUBLISHED)
+    #     Comments
+    comments = post.comments.filter(
+        active=True,
+    )
+    # Comment's form:
+    form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {
+                      'post': post,
+                      'comments': comments,
+                      'form': form
+                  }
+                  )
 
 
 class PostListView(ListView):
@@ -81,3 +93,30 @@ def post_share(request, post_id):
         }
     )
 
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+
+    comment = None
+    form = CommentForm(
+        data=request.POST
+    )
+    if form.is_valid():
+        comment = form.save(
+            commit=False
+        )
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
+        }
+    )

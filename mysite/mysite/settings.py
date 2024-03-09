a7f6fbf6e7a83ec27f2cacb946d4b08d70b34d8b
Valkhnut <dhjob@yandex.by>
2024-03-09 13:33:07 +0300

    added share by mail functional

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -128,6 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'djangosite.test.mailler@gmail.com'
-EMAIL_HOST_PASSWORD = 'ingd dljn ohhm ymmw'
+EMAIL_HOST_PASSWORD = '*** *** *** *** ***'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True

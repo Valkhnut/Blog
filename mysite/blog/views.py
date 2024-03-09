84a7e2f58d885ed4cc726faa783c5573425440d1
Valkhnut <dhjob@yandex.by>
2024-03-09 03:49:40 +0300

    resolve some bugs

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+
 
 from .models import Post
 
@@ -8,19 +8,15 @@ from .models import Post
 def post_list(request):
     posts = Post.published.all()
     return render(request,
-                  'blog/list.html',
+                  'blog/post/list.html',
                   {'posts': posts,}
                   )
 
 
-def post_detail(request):
-    post = get_object_or_404(
-        Post,
-        id=id,
-        status=Post.Status.PUBLISHED
-    )
-    return render(
-        request,
-        'blog/post/detail.html',
-        {'post': post,}
-    )
+def post_detail(request, id):
+    post = get_object_or_404(Post,
+                             id=id,
+                             status=Post.Status.PUBLISHED)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})

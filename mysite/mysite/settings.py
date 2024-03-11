48d7d8e212e926f2d345fb04849c1afdec96d7ec
Valkhnut <dhjob@yandex.by>
2024-03-11 22:52:35 +0300

    connect to PostgreSQL and create Search's form

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'your_password',
+        'PASSWORD': 'd13101995',
     }
 }
 
@@ -137,8 +137,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email server configuration
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'your_mail_login'
-EMAIL_HOST_PASSWORD = 'your_mail_password'
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv('MAIL_HOST')
+    # 'smtp.gmail.com'
+EMAIL_HOST_USER = os.getenv('MAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('MAIL_HOST_PASSWORD')
+EMAIL_PORT = os.getenv('MAIL_PORT')
 EMAIL_USE_TLS = True
